ee516d8a56fe7044258a627cba124bcd3c6c7225
lenechapion <ryokitta326@gmail.com>
2024-01-10 20:22:49 +0900

    ８章コード8-13まで

--- src/java8/main.java
+++ src/java8/main.java
@@ -3,8 +3,18 @@ package java8;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		//1.勇者を生成
+		Hero h = new Hero();
+		//2.フィールドに初期値を設定
+		h.name = "ミナト";
+		h.hp = 100;
+		System.out.println("勇者" + h.name + "を生み出しました！");
+		
+		//勇者のメソッドを呼び出す
+		h.sit(5);
+		h.slip();
+		h.sit(25);
+		h.run();
 	}
 
 }
