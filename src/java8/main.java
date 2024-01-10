905b1a89d94721e4d957ec9cce07fde1550dd120
lenechapion <ryokitta326@gmail.com>
2024-01-10 20:40:57 +0900

    ８章コード8-16まで

--- src/java8/main.java
+++ src/java8/main.java
@@ -8,12 +8,20 @@ public class main {
 		//2.フィールドに初期値を設定
 		h.name = "ミナト";
 		h.hp = 100;
-		System.out.println("勇者" + h.name + "を生み出しました！");
 		
-		//勇者のメソッドを呼び出す
-		h.sit(5);
+		Matango m1 = new Matango();
+		m1.hp = 50;
+		m1.suffix = 'A';
+		
+		Matango m2 = new Matango();
+		m2.hp = 48;
+		m2.suffix = 'B';
+		
+		
+		//冒険のはじまり
 		h.slip();
-		h.sit(25);
+		m1.run();
+		m2.run();
 		h.run();
 	}
 
